7d63388faec8be54c80bf6df80a7c75f98d58bf9
Joliosaurio <julio_sanchez@outlook.com>
2017-04-04 20:35:50 -0500

    Add files via upload

---
+++ Tarea1/Tarea.cpp
@@ -0,0 +1,16 @@
+#include <iostream>;
+
+void main() {
+	int Valorx, numero = 5; //Numero es un valor que puede ser cambiado.
+	for (Valorx = numero; Valorx < 10; ++Valorx)
+	{
+		
+		std::cout << "Valor de la suma = " << Valorx << std::endl;
+		//En el ciclo, este revisa si el valor asignado a "Valorx" es menor que diez, si es el caso...
+		//...se hace la adición a "Valorx"
+		system("pause");
+		
+
+	}
+
+}
